b890d698552a251ce4f891662ac20823f04a0fde
ArianFrutoss <arian.frutos@ikasle.aeg.eus>
2024-10-22 13:51:23 +0200

    [ER6-252]ADD - obtain the mac of the ESP32

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,5 +1,15 @@
+#include <WiFi.h>
+#include <esp_wifi.h>
+
 void setup(){
 
+  Serial.begin(115200);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.STA.begin();
+ 
+  Serial.print("\nDefault ESP32 MAC Address: ");
+  Serial.println(WiFi.macAddress());
 }
  
 void loop(){
