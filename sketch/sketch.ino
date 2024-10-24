3c4e6fbe99e2b0b6760debcf1dad3d59bee74cb1
ArianFrutoss <arian.frutos@ikasle.aeg.eus>
2024-10-24 13:47:47 +0200

    [ER6-260]ADD - connect the ESP32 with mqtt mosquito broker

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,34 +1,75 @@
 #include <WiFi.h>
 #include <esp_wifi.h>
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
+
 #include "../arduino_secrets.h"
 
 const char* ssid = SECRET_SSID;
-
 const char* password = SECRET_PSW;
 
-void setup(){
+const char* mqtt_server = SECRET_SERVER;
+const int mqtt_port = SECRET_PORT;
 
-  Serial.begin(115200);
+WiFiClient espClient;
 
-  WiFi.mode(WIFI_STA);
-  WiFi.STA.begin();
- 
-  Serial.print("\nDefault ESP32 MAC Address: ");
-  Serial.println(WiFi.macAddress());
+PubSubClient client(espClient);
+
+void reconnect(){
+
+  while(!client.connected()){
+
+    Serial.print("Attempting MQTT connection...");
+    
+    if(client.connect("ESP32CosaNostra")){
+
+      Serial.println("Connected");
+    }
+
+    else{
+
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println("try again in 5 seconds");
+
+      delay(5000);
+    }
+  }
+}
+
+void setup_wifi(){
+  
+  delay(10);
 
   WiFi.begin(ssid, password);
+
+  Serial.println(ssid);
 
   while (WiFi.status() != WL_CONNECTED) {
 
     delay(500);
 
     Serial.println("Connecting to WiFi..");
-
   }
 
   Serial.println("Connected to the WiFi network");
 }
+
+void setup(){
+
+  Serial.begin(115200);
+
+  setup_wifi();
+
+  client.setServer(mqtt_server, mqtt_port);
+}
  
 void loop(){
+  
+  if(!client.connected()){
 
+    reconnect();
+  }
+
+  client.loop();
 }
